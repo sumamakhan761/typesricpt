f40a226636250fa8689bab1de5835ded24e16ba9
sumama <sumamakhan761>
2024-11-02 19:17:59 +0530

    array

---
+++ Array/array.ts
@@ -0,0 +1,22 @@
+const supperhero: string[] = []
+const heropower: number[] = []
+// const heropower : Array<number> = []
+
+type User = {
+  name: string
+  isActive: boolean
+}
+
+const alluser: User[] = []
+
+const MLmodel: number[][] = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 7],
+  [8, 9, 0, -1]
+]
+supperhero.push("supperhero")
+// heropower.push("supperhero") this give error bcz we want number ans you give string 
+heropower.push(761);
+
+alluser.push({ name: "", isActive: true });
+
